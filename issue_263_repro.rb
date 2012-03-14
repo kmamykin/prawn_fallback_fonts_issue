cf87a21714b73773e085ede3887e7e261d5f059a
Kliment Mamykin <kmamyk@yahoo.com>
2012-03-14 17:14:50 -0400

    experimenting with formatted_text_box. Looks like fallback_fonts work

--- issue_263_repro.rb
+++ issue_263_repro.rb
@@ -1,22 +1,28 @@
+# encoding: UTF-8
 require 'prawn'
+require 'prawn/measurement_extensions'
 
 Prawn::Document.generate "fallback_fonts.pdf" do
   font_families.update("DroidSansFallback" => {
+    :bold   => { :file => "./DroidSansFallback.ttf", :font => "DroidSansFallback" },
     :normal => { :file => "./DroidSansFallback.ttf", :font => "DroidSansFallback" }
   })
   font("Times-Roman") # just to see which font is actually being used, DroidSans is too close looking to Helvetica
+
   fallback_fonts([])
   text "fallback_fonts is empty, :style=>:bold works"
   text "Some text that is not bold", :size => 40
   text "Some text that should be bold", :style => :bold, :size => 40
-  fallback_fonts(["DroidSansFallback"])
+
+  fallback_fonts(%W(DroidSansFallback))
   text "fallback_fonts is DroidSansFallback, :style=>:bold does not work"
   text "Some text that is not bold", :size => 40
   # The following text is drawn normal style, even though the fallback font is not even used
   text "Some text that should be bold", :style => :bold, :size => 40 
 
-  move_down 20
-  font "DroidSansFallback" do
-    text "Some text in DroidSansFallback", :size => 30
-  end
+  text "using formatted_text_box works with fallback_fonts and :style=>:bold"
+  formatted_text_box [
+    {:text=>"Normal text ", :styles=>[:normal], :size=>30},
+    {:text=>"Bold text 虫爬小个写事难么", :styles=>[:bold], :size=>30}],
+    :at=>[0, 6.5.in], :width=>8.in, :height=>1.in
 end
